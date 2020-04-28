9885a28eb4ddd20d4522d6d16adb0dc6931028bc
Minh Tuan VO <vominhtuan164@gmail.com>
2020-04-28 11:52:19 +0200

    add speech recognition key

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,13 +11,20 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/MainLayout.fxml"));
+        Scene scene = new Scene(root,1280, 720);
+        scene.getStylesheets().add(getClass().getResource("/css/style.css").toExternalForm());
         primaryStage.setTitle("Virtual Assistant");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(scene);
         primaryStage.show();
+
     }
 
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public void openLinkInBrowser(String url) {
+        getHostServices().showDocument(url);
     }
 }
--- src/sample/controller/MainController.java
+++ src/sample/controller/MainController.java
@@ -1,6 +1,10 @@
 package sample.controller;
 
 import com.darkprograms.speech.microphone.Microphone;
+import com.darkprograms.speech.recognizer.GSpeechDuplex;
+import com.darkprograms.speech.recognizer.GSpeechResponseListener;
+import com.darkprograms.speech.recognizer.GoogleResponse;
+
 import com.jfoenix.controls.*;
 import com.jfoenix.transitions.hamburger.HamburgerBackArrowBasicTransition;
 import javafx.animation.TranslateTransition;
@@ -17,15 +21,20 @@ import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 import net.sourceforge.javaflacencoder.FLACFileWriter;
+import sample.Main;
 
+import javax.sound.sampled.LineUnavailableException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainController implements Initializable {
+public class MainController implements Initializable, GSpeechResponseListener {
 
     // Initial variable
     private int clickCounter = 0;
     private Microphone micro = new Microphone(FLACFileWriter.FLAC);
+    public static final String API_KEY = "AIzaSyClXZmx76lcqzkDbXbgxRS6dMmOBBYysrA";
+    private GSpeechDuplex duplex = new GSpeechDuplex(API_KEY);
+    private static Main mainInstance = new Main();
 
     @FXML
     private AnchorPane root;
@@ -51,14 +60,19 @@ public class MainController implements Initializable {
     @FXML
     private VBox drawer;
 
-//    @FXML
-//    private StackPane backgroundStackPane;
+    @FXML
+    private StackPane backgroundStackPane;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         System.out.println("initialize");
 
+        // Initial, StackPane is hided
+        backgroundStackPane.setVisible(false);
+        backgroundStackPane.setOpacity(0.8);
+
+        // Hamburger transition
         HamburgerBackArrowBasicTransition transition = new HamburgerBackArrowBasicTransition(hamAnimation);
         transition.setRate(-1);
 
@@ -139,48 +153,110 @@ public class MainController implements Initializable {
     private void handleAboutButtonClicked(ActionEvent event) {
         System.out.println("About Button Clicked.");
 
-//        JFXDialogLayout aboutDialogLayout = new JFXDialogLayout();
-//        aboutDialogLayout.setHeading(new Text("About"));
-//        aboutDialogLayout.setBody(new Text("Made by : Minh Tuan VO - 4ASTI - Option: Commerce Electronique"));
-//        aboutDialogLayout.setOpacity(1);
-//        aboutDialogLayout.setStyle("-fx-background-color: rgba(250,250,250,0.15)");
-//        aboutDialogLayout.setStyle("-fx-background-radius: 20%");
-//        aboutDialogLayout.applyCss();
-//
-//        JFXDialog aboutDialog = new JFXDialog(backgroundStackPane,
-//                aboutDialogLayout,
-//                JFXDialog.DialogTransition.CENTER);
-//        backgroundStackPane.setVisible(true);
-//
-//        JFXButton aboutDialogOkayButton = new JFXButton("Okay");
-//
-//        aboutDialog.setOnDialogOpened( aboutDialogOpened -> {
-//            System.out.println("About Dialog Box Opened.");
-//            aboutDialogOkayButton.setOnAction((aboutDialogOpenedEvent) -> {
-//
-//                aboutDialog.close();
-//                backgroundStackPane.setVisible(false);
-//            });
-//
-//            aboutDialogLayout.setActions(aboutDialogOkayButton);
-//
-//
-//        });
-//
-//        aboutDialog.show();
+        JFXDialogLayout aboutDialogLayout = new JFXDialogLayout();
+        aboutDialogLayout.setHeading(new Text("About"));
+        aboutDialogLayout.setBody(new Text("Minh Tuan VO - 4ème année en STI - Option: Commerce Electronique"));
+
+        JFXDialog aboutDialog = new JFXDialog(backgroundStackPane,
+                aboutDialogLayout,
+                JFXDialog.DialogTransition.CENTER);
+        backgroundStackPane.setVisible(true);
+        JFXButton aboutDialogOkayButton = new JFXButton("Okay");
+        aboutDialog.setOnDialogOpened( aboutDialogOpened -> {
+            System.out.println("About Dialog Box Opened.");
+            aboutDialogOkayButton.setOnAction((aboutDialogOpenedEvent) -> {
+
+                aboutDialog.close();
+                backgroundStackPane.setVisible(false);
+            });
+            aboutDialogLayout.setActions(aboutDialogOkayButton);
+        });
+        aboutDialog.show();
     }
 
     @FXML
     private void handleStartButtonClicked(ActionEvent event) {
+        System.out.println("Start Button Clicked");
+        startRec();
+
+    }
+
+    private void startRec() {
+        duplex.setLanguage("en");
+        duplex.addResponseListener(new GSpeechResponseListener() {
+
+            String old_text = "";
+
+            @Override
+            public void onResponse(GoogleResponse googleResponse) {
+                System.out.println("In response");
+                String output = "";
+                output = googleResponse.getResponse();
+
+                if (googleResponse.getResponse() == null) {
+
+                    this.old_text = recTextView.getText();
+
+                    if (this.old_text.contains("(")) {
+
+                        this.old_text = this.old_text.substring(0, this.old_text.indexOf('('));
+                    }
+                    System.out.println("Paragraph Line Added");
+
+                    this.old_text = (recTextView.getText() + "\n");
+                    this.old_text = this.old_text.replace(")", "").replace("( "
+                            , "");
+
+                    recTextView.setText(this.old_text);
+                    return;
+                }
+                if (output.contains("(")) {
+
+                    output = output.substring(0, output.indexOf('('));
+                }
+                if (!googleResponse.getOtherPossibleResponses().isEmpty()) {
+
+                    output = output + " (" + (String) googleResponse.getOtherPossibleResponses().get(0) + ")";
+                }
+
+                System.out.println("||-> " + output);
+                recTextView.setText("");
+
+                recTextView.appendText(this.old_text);
+                recTextView.appendText(output);
+            }
+        });
+        startSpeechRecognition();
+    }
+
+    private void startSpeechRecognition() {
+
+        new Thread(
+                () -> {
+                    try {
+
+                        duplex.recognize(micro.getTargetDataLine(), micro.getAudioFormat());
+
+                    } catch (InterruptedException | LineUnavailableException exception) {
+
+                        System.out.println("Exception caused : " + exception.getMessage());
+                    }
+                }).start();
     }
 
     @FXML
     private void handleSettingsButtonClicked(ActionEvent event) {
+        System.out.println("Setting Button Clicked");
     }
 
     @FXML
     private void handleUserButtonClicked(ActionEvent event) {
         System.out.println("User Button Clicked.");
+        mainInstance.openLinkInBrowser("https://github.com/vominhtuan164");
+    }
 
+    @Override
+    public void onResponse(GoogleResponse googleResponse) {
+        // Do nothing
     }
 }
